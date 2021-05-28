6854529def9367b6b1dbadf6762db4f8afdd806e
Erdaulet2000 <e.serikbai.e@gmail.com>
2021-05-28 06:09:37 +0600

    flutter

---
+++ flutte/android/app/src/main/java/yourcompany/flutte/MainActivity.java
@@ -0,0 +1,6 @@
+package yourcompany.flutte;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutte/lib/constant/data_json.dart
@@ -0,0 +1,201 @@
+List menuItems = [
+  "Costume",
+  "Footwear",
+  "Shirt",
+  "Clock",
+  "Tie",
+  "Belt",
+  "Pants",
+  "Coat",
+  "Backpack",
+  "Jeans"
+];
+
+
+List dataItems = [
+  {
+    "id" : 1,
+    "code" : "Coat",
+    "name" : " High Quality Mens Jackets For Men New Casual Fashion Solid Color Jacket Coat Classic Jacket Brand Boys Coat Plus Size",
+    "img" : "https://wowtrends.ru/wp-content/uploads/2019/10/modnaya-muzhskaya-odezhda-57.jpg",
+    "price" : 150.00,
+    "promotionPrice" : 110.00,
+    "color" : [
+      {
+        "id" : 1,
+        "value" : "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F4f%2F5b%2F4f5b07ecebf861ebf6109391560a78bea92a89c7.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_jacketscoats_coats%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
+      },
+      {
+        "id" : 2,
+        "value" : "https://aliradar.com/api/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FH6accc6292afb4b72a90af439b2ee00aaP%2F-.jpg_220x220.jpg_Q70.jpg"
+      }
+    ],
+    "size" :[
+      {
+        "id" : 1,
+        "value" : "S"
+      },
+      {
+        "id" : 2,
+        "value" : "M"
+      },
+      {
+        "id" : 3,
+        "value" : "L"
+      }
+    ]
+  },
+  {
+    "id" : 2,
+    "code" : "Pants",
+    "name" : "KB Cargo Pants Men Lightweight Tactical Breathable Casual Army Military Pants Long Waterproof Quick Dry Summer",
+    "img" : "https://img1.wildberries.kz/c516x688/new/10350000/10353765-1.jpg",
+    "price" : 30.00,
+    "promotionPrice" : 20.00,
+    "color" : [
+      {
+        "id" : 1,
+        "value" : "https://mir-bruk.ru/wa-data/public/shop/products/81/06/681/images/2593/2593.970x0@2x.jpg"
+      },
+      {
+        "id" : 2,
+        "value" : "https://etelier.by/upload/iblock/223/K362-SERYY_2_min.jpg"
+      }
+    ],
+    "size" : [
+      {
+        "id" : 1,
+        "value" : "FREE-SIZE"
+      }
+    ]
+  },
+  {
+    "id" : 3,
+    "code" : "Shirt",
+    "name" : "HYBSKR Shirt Men Patchwork Casual Oversized Loose Chemise Harajuku Top Black / White",
+    "img" : "https://cutur.ru/wp-content/uploads/_pu/3/43484582.jpg",
+    "price" : 16.5,
+    "promotionPrice" : 12.5,
+    "color" : [
+      {
+        "id" : 1,
+        "value" : "https://i.pinimg.com/originals/f3/9c/2d/f39c2d64a77bc23cd753cd06cf51a157.jpg"
+      },
+      {
+        "id" : 2,
+        "value" : "https://images.satu.kz/46284867_w640_h640_muzhskaya-rubashka-v.jpg"
+      }
+    ],
+    "size" : [
+      {
+        "id" : 1,
+        "value" : "S"
+      },
+      {
+        "id" : 2,
+        "value" : "M"
+      },
+      {
+        "id" : 3,
+        "value" : "L"
+      }
+    ]
+  },
+  {
+    "id" : 4,
+    "code" : "Clock",
+    "name" : "EPOZZ wristwatch men's electronic, sports LED, military style, quartz, stainless steel, silver",
+    "img" : "https://static-sl.insales.ru/images/products/1/5954/151435074/medium_muski-sat-fossil-FS5304.jpg",
+    "price" : 175.00,
+    "promotionPrice" : 110.00,
+    "color" : [
+      {
+        "id" : 1,
+        "value" : "https://static-sl.insales.ru/images/products/1/7853/259776173/medium_MTP-1374D-1A.jpg"
+      },
+      {
+        "id" : 2,
+        "value" : "https://images.satu.kz/153666264_w200_h200_muzhskie-chasy-rado.jpg"
+      }
+    ],
+
+    "size" : [
+      {
+        "id" : 1,
+        "value" : "FREE-SIZE"
+      }
+    ]
+  },
+  {
+    "id" : 5,
+    "code" : "Backpack",
+    "name" : "15.6 inch laptop backpack with usb charging, backpacks for men and women, schoolbag, cloth bag, shoulder bags",
+    "img" : "https://www.lakestone.kz/image/cache/catalog/Adams%20Black/IMG_7262-350x350.jpg",
+    "price" : 50.00,
+    "promotionPrice" : 45.00,
+     "color" : [
+      {
+        "id" : 1,
+        "value" : "https://www.lakestone.kz/image/cache/catalog/%20Adams%20Black%20Grey/L_8745-350x350.jpg"
+      },
+      {
+        "id" : 2,
+        "value" : "https://frankfurt.apollo.olxcdn.com/v1/files/u7olfckq7wp62-KZ/image;s=644x461"
+      }
+      ],
+    "size" : [
+      {
+        "id" : 1,
+        "value" :"25"
+      },
+      {
+        "id" : 2,
+        "value" :"26"
+      },
+      {
+        "id" : 3,
+        "value" :"27"
+      },
+      {
+        "id" : 4,
+        "value" :"28"
+      },
+    ]
+  },
+  {
+    "id" : 6,
+    "code" : "Footwear",
+    "name" : "Mens shoes mens casual shoes, novelty, made of artificial leather; Male shoes",
+    "img" : "https://i.pinimg.com/564x/5a/02/21/5a0221ff6cab5397f2421d13f1a93ca5.jpg",
+    "price" : 100.00,
+    "promotionPrice" : 85.00,
+    "color" : [
+      {
+        "id" : 1,
+        "value" : "https://frankfurt.apollo.olxcdn.com/v1/files/zjlknka3hmv7-KZ/image;s=640x640"
+      },
+      {
+        "id" : 2,
+        "value" : "https://images.satu.kz/118210787_w640_h640_muzhskaya-obuv.jpg"
+      }
+    ],
+    "size" : [
+      {
+        "id" : 1,
+        "value" :"25"
+      },
+      {
+        "id" : 2,
+        "value" :"26"
+      },
+      {
+        "id" : 3,
+        "value" :"27"
+      },
+      {
+        "id" : 4,
+        "value" :"28"
+      },
+    ]
+  }
+];
